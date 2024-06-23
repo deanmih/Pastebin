f7a42504405a41ed706477d85a13fb469816c43f
deanmih <153102912+deanmih@users.noreply.github.com>
2024-06-23 15:41:31 +0300

    collectTextsController.java

--- collectTextsController.java
+++ collectTextsController.java
@@ -9,47 +9,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
-
 @RestController
 public class collectTextsController {
     private TreeMap<String, String> textMap = new TreeMap<>();
     private String key;
     
     @PostMapping("/txt")
-    public void addTxt(@RequestBody txtCollector txtCollector) {
-        
+    public void addTxt(@RequestBody txtCollector txtCollector) { 
         String key = txtCollector.getKey();
         String val = txtCollector.getVal();
         textMap.put(key, val);
-
-        //System.out.println(textMap);
-       
     }
 
     @PostMapping("/sentKey")
     public void postMethodName(@RequestBody returnKey returnKey) {
         this.key = returnKey.getKey();
-        //System.out.println("PRESSED: " + key + " " + textMap.get(key));
     }
     
-
     @GetMapping("/getValue")
     public String getTxt(@RequestParam String key) {
         String res = textMap.get(key);
-        //System.out.println("ASTA E VALOAREA: " + res);
         return res;
     }
 
     @GetMapping("/getKey")
     public String printVal() {
         return key;
-    }
-    
-
-
-    
-    
-    
+    }  
 }
