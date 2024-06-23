1a50cc853342aa854a9224e906ecc5161b717399
deanmih <153102912+deanmih@users.noreply.github.com>
2024-06-23 15:26:26 +0300

    Add files via upload

---
+++ returnKey.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class returnKey {
+    private String key;
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+    
+    public String getKey() {
+        return key;
+    }
+
+}
